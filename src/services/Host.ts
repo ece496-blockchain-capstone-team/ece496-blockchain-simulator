3bc4fc0e79e19fbfc3ce93ea87a4b66292e7948b
Daniel Liang <daniel65@live.ca>
2021-12-24 16:10:56 -0500

    #2 Rewrote the host class functions so interaction between hosts is controlled outside and removed demo code so the project builds

--- src/services/Host.ts
+++ src/services/Host.ts
@@ -1,114 +1,91 @@
-import React from 'react';
-import ConnectionObj from './Connections';
-import HostRole from './HostType';
 import ChainObj from './Chain';
 import Actions from './actions';
+import BlockObj from './Block';
 
 export default class Host {
   nodeId: number;
   nodeName: string;
   userStake: number;
-  role: HostRole;
-  locationIds: 0;
-  connections: ConnectionObj[] = [];
+  locationId: number;
   connectedNodes: Host[] = [];
-  lastValdationTime: 0;
-  lastUpdatedTime: 0;
-  lastActionTime: 0;
-  validatorProbability: 0;
+  lastLeaderTime: number = 0;
+  lastActionTime: number = 0;
   chain: ChainObj;
-  actionQueue: Actions;
+  actions: Actions;
 
-  constructor(id: number, role: HostRole, connectedNodes: number[]) {
+  constructor(id: number, name: string, stake: number, connections: Host[]) {
     this.nodeId = id;
-    this.nodeName = 'Test';
-    this.userStake = 0;
-    this.role = role;
+    this.nodeName = name;
+    this.userStake = stake;
+    this.connectedNodes = connections;
     this.chain = new ChainObj();
-    // set connections
-    for (let i = 0; i < connectedNodes.length; i++) {
-      let tempConnections = new ConnectionObj(id, connectedNodes[i], 1);
-      this.connections.push(tempConnections);
-    }
-    this.actionQueue = new Actions();
+    this.actions = new Actions();
   }
 
-  addConnectedNodes(node: Host) {
+  addConnectedNode(node: Host) {
     this.connectedNodes.push(node);
   }
 
-  takeAction(time: number) {
-    let actions = this.actionQueue.getNextAction(time);
-    if (actions.length) {
-      console.log(actions);
-      for (let i = 0; i < actions.length; i++) {
-        if (actions[i][0] === 'add-block') {
-          this.addBlock(time);
-        } else if (actions[i][0] === 'update-chain') {
-          this.updateChain(time, actions[i][1]);
-        }
-      }
-      this.actionQueue.removeAction(time);
+  removeConnectedNode(node: Host) {
+    const index = this.connectedNodes.indexOf(node);
+    if (index > -1) {
+      this.connectedNodes.splice(index, 1);
+      return true;
     }
+    return false;
+  }
+
+  setStake(stake: number) {
+    this.userStake = stake;
+  }
+
+  addAction(time: number, action: any[]) {
+    this.actions.addAction(time, action);
+    this.lastActionTime = time;
+  }
+
+  validateBlock(block: BlockObj) {
+    return true;
+  }
+
+  addBlock(block: BlockObj) {
+    this.chain.addBlock();
+  }
+
+  proposeBlock(time: number) {
+    this.lastLeaderTime = time;
+    return new BlockObj();
+  }
+
+  getAction(time: number) {
+    return this.actions.getNextAction(time);
   }
 
   getId() {
     return this.nodeId;
   }
 
-  removeDup(element: any, index: any, array: any) {
-    return element !== this.nodeId;
+  getName() {
+    return this.nodeName;
   }
 
-  getConnectedNodeIds() {
-    let tempNodes: number[] = [];
-    for (let i = 0; i < this.connections.length; i++) {
-      for (let j = 0; j < this.connections[i].nodeIds.length; j++) {
-        if (this.connections[i].nodeIds[j] !== this.nodeId) {
-          tempNodes.push(this.connections[i].nodeIds[j]);
-        }
-      }
-    }
-    return tempNodes.join(', ');
+  getStake() {
+    return this.userStake;
   }
 
-  addBlockAction(time: number) {
-    let temp = [];
-    temp.push('add-block');
-    this.actionQueue.addAction(time, temp);
-  }
-
-  addBlock(time: number) {
-    console.log('adding block');
-    this.chain.addBlock();
-    this.brodcastTransaction(time, this.chain);
+  getConnectedNodes() {
+    return this.connectedNodes;
   }
 
   getChain() {
     return this.chain;
   }
 
-  updateChainAction(time: number, vrsn: ChainObj) {
-    let temp = [];
-    temp.push('update-chain');
-    temp.push(vrsn);
-    this.actionQueue.addAction(time, temp);
+  getLastLeaderTime() {
+    return this.lastLeaderTime;
   }
 
-  updateChain(time: number, vrsn: ChainObj) {
-    if (this.chain.version >= vrsn.version) {
-      return true;
-    }
-    this.chain.updateChain(vrsn);
-    this.brodcastTransaction(time, vrsn);
-    return true;
+  getLastActionTime() {
+    return this.lastActionTime;
   }
-
-  brodcastTransaction(time: number, vrsn: ChainObj) {
-    for (let i = 0; i < this.connectedNodes.length; i++) {
-      this.connectedNodes[i].updateChainAction(time + 5, vrsn);
-    }
-  }
-
-  validateBlock() {}
 }
--- src/services/Network.ts
+++ src/services/Network.ts
@@ -11,37 +11,6 @@ export default class NetworkObj {
   constructor(numNodes: number) {
     this.networkId = 1;
     this.timeCounter = 1;
-    // for (let nodeid = 0; nodeid < numNodes; nodeid++){
-    //     let hostRole = new HostRole("general");
-    //     let tempHost = new Host(nodeid, hostRole, []);
-    //     this.nodeList.push(tempHost);
-    // };
-
-    let hostRole = new HostRole('general');
-    let tempHost = new Host(0, hostRole, [2, 4]);
-    let tempHost2 = new Host(1, hostRole, [2, 0, 3]);
-    let tempHost3 = new Host(2, hostRole, [1, 4, 0]);
-    let tempHost4 = new Host(3, hostRole, [1, 4, 2]);
-    let tempHost5 = new Host(4, hostRole, [2, 3]);
-    tempHost.addConnectedNodes(tempHost3);
-    tempHost.addConnectedNodes(tempHost5);
-    this.nodeList.push(tempHost);
-    tempHost2.addConnectedNodes(tempHost3);
-    tempHost2.addConnectedNodes(tempHost);
-    tempHost2.addConnectedNodes(tempHost4);
-    this.nodeList.push(tempHost2);
-    tempHost3.addConnectedNodes(tempHost2);
-    tempHost3.addConnectedNodes(tempHost5);
-    tempHost3.addConnectedNodes(tempHost);
-    this.nodeList.push(tempHost3);
-    tempHost4.addConnectedNodes(tempHost2);
-    tempHost4.addConnectedNodes(tempHost5);
-    tempHost4.addConnectedNodes(tempHost3);
-    this.nodeList.push(tempHost4);
-    tempHost5.addConnectedNodes(tempHost3);
-    tempHost5.addConnectedNodes(tempHost4);
-    this.nodeList.push(tempHost5);
-
     this.settings.stakeDeposit = null;
     this.settings.valSelectionAlgo = null;
     this.settings.maliciousBehavior = null;
@@ -53,7 +22,7 @@ export default class NetworkObj {
     this.timeCounter += steps;
     console.log(this.timeCounter);
     for (let i = 0; i < this.nodeList.length; i++) {
-      this.nodeList[i].takeAction(this.timeCounter);
+      this.nodeList[i].getAction(this.timeCounter);
     }
   }
 
