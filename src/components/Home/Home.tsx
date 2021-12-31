abe4178828f15e8b7a665010e40b5b8fe946813a
Daniel Liang <daniel65@live.ca>
2021-12-31 16:59:32 -0500

    #6 Created div elements that are hid/shown by the buttons

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,55 @@
 import React from 'react';
-import { Button, Grid } from '@chakra-ui/react';
+import { Button, Grid, Heading } from '@chakra-ui/react';
 
 export default function Home() {
+  function toggleItemDisplay(itemName: string) {
+    let options = document.getElementById('options');
+    let item = document.getElementById(itemName);
+    if (options && item) {
+      options.hidden = !options.hidden;
+      item.hidden = !item.hidden;
+    }
+  }
+
   return (
     <div className="HomePage">
-      <div className="options">
+      <div id="options">
         <Grid templateColumns="repeat(2, 2fr)" gap={6} width={500}>
-          <Button size="lg">Introduction</Button>
-          <Button size="lg">Simulation Settings</Button>
-          <Button size="lg">Create Network</Button>
-          <Button size="lg">Start Simulation</Button>
+          <Button size="lg" onClick={() => toggleItemDisplay('introduction')}>
+            Introduction
+          </Button>
+          <Button size="lg" onClick={() => toggleItemDisplay('network-settings')}>
+            Create Network
+          </Button>
+          <Button size="lg" onClick={() => toggleItemDisplay('settings')}>
+            Simulation Settings
+          </Button>
+          <Button size="lg" onClick={() => alert('Not implemented yet')}>
+            Start Simulation
+          </Button>
         </Grid>
+      </div>
+      <div id="introduction" hidden>
+        <Heading size="lg">Introduction</Heading>
+        <p>Welcome to the Blockchain Simulator.</p>
+        <br />
+        <Button size="sm" onClick={() => toggleItemDisplay('introduction')}>
+          Back
+        </Button>
+      </div>
+      <div id="network-settings" hidden>
+        <Heading size="lg">Create Network</Heading>
+        <br />
+        <Button size="sm" onClick={() => toggleItemDisplay('network-settings')}>
+          Save
+        </Button>
+      </div>
+      <div id="settings" hidden>
+        <Heading size="lg">Simulation Settings</Heading>
+        <br />
+        <Button size="sm" onClick={() => toggleItemDisplay('settings')}>
+          Save
+        </Button>
       </div>
     </div>
   );
