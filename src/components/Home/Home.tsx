8580713931ae9a70d93bb4415389a25ab1d95882
David Yee <github@davidyee.dev>
2022-01-18 15:25:58 -0800

    #12 Added devtools to view store state

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { Grid, GridItem, Heading, Divider } from '@chakra-ui/react';
+
+import { createDevTools } from '@redux-devtools/core';
+import { LogMonitor } from '@redux-devtools/log-monitor';
+
+const DevTools = createDevTools(<LogMonitor theme="tomorrow" />);
+
 export default function Home() {
   return (
-    <div className="HomePage">
-      <p> This is a test paragraph. </p>
-    </div>
+    <Grid w="100%" p={4} gap={4}>
+      <Heading> Redux Store Current State </Heading>
+      <Divider />
+      <DevTools />
+    </Grid>
   );
 }
