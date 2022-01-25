0389a40957a771cef643f252a5b8ce3717cc9032
David Yee <github@davidyee.dev>
2022-01-24 17:23:53 -0800

    #12 Removed redux-devtool components since they were not working as intended, added instructions for how to install browser extension

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 
 import { Grid, GridItem, Heading, Divider } from '@chakra-ui/react';
 
-import { createDevTools } from '@redux-devtools/core';
-import { LogMonitor } from '@redux-devtools/log-monitor';
-
-const DevTools = createDevTools(<LogMonitor theme="tomorrow" />);
-
 export default function Home() {
   return (
     <Grid w="100%" p={4} gap={4}>
-      <Heading> Redux Store Current State </Heading>
-      <Divider />
-      <DevTools />
+      <Heading> Home </Heading>
     </Grid>
   );
 }
