7d2c457d5bb8247f64f7b7b21c10f9f067afda40
David Yee <github@davidyee.dev>
2022-01-24 19:25:34 -0800

    #12 Fixed all test cases

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '../Home';
 
-test('renders test paragraph for homepage', () => {
+test('home is rendered', () => {
   render(<Home />);
-  const element = screen.getByText(/This is a test paragraph/i);
-  expect(element).toBeInTheDocument();
 });
