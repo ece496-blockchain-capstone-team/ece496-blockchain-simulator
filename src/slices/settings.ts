c8766020707cd44ff7b863003d3aa332113299d8
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2022-02-24 20:26:27 -0500

    #6 Replaced global data structure with Redux slice

--- src/slices/settings.ts
+++ src/slices/settings.ts
@@ -3,17 +3,30 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const settings = createSlice({
   name: 'settings',
   initialState: {
-    stakeDeposit: null,
-    valSelectionAlgo: null,
-    maliciousBehavior: null,
-    antiMaliciousAlgo: null,
-    maxBlockSize: null,
+    stake: 1,
+    electionAlgo: 1,
+    votingPower: 1,
+    antiMaliciousAlgo: 1,
+    blockSize: 10,
   },
   reducers: {
-    setSetting: (state, action: PayloadAction<{ name: any; value: any }>) => {
-      // state[action.payload.name] = action.payload.value;
+    setStake: (state, action: PayloadAction<number>) => {
+      state.stake = action.payload;
+    },
+    setElectionAlgo: (state, action: PayloadAction<number>) => {
+      state.electionAlgo = action.payload;
+    },
+    setVotingPower: (state, action: PayloadAction<number>) => {
+      state.votingPower = action.payload;
+    },
+    setAntiMaliciousAlgo: (state, action: PayloadAction<number>) => {
+      state.antiMaliciousAlgo = action.payload;
+    },
+    setBlockSize: (state, action: PayloadAction<number>) => {
+      state.blockSize = action.payload;
     },
   },
 });
 
+// export const { setStake, setElectionAlgo, setMaliciousBehavior, setAntiMaliciousAlgo, setBlockSize } = settings.actions;
 export default settings;
