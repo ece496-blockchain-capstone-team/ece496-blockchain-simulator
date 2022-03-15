ebc404eeef217a163817894d6b867b5b6bbd3afe
Chenyan Wang <chenyan.wang@mail.utoronto.ca>
2022-03-14 20:06:50 -0400

    Temp sidebar WIP

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
 
 import Home from './components/Home';
 import Network from './components/Network';
@@ -17,17 +18,19 @@ export default function App() {
   return (
     <ChakraProvider>
       <NavBar />
-      <div className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/network" element={<Network />} />
-          <Route path="/host" element={<Host />} />
-          <Route path="/chain" element={<Chain />} />
-          <Route path="/block" element={<Block />} />
-          <Route path="/simulation-settings" element={<SimulationSettings />} />
-          <Route path="/metrics" element={<Metrics />} />
-        </Routes>
-      </div>
+      <SideBar>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/network" element={<Network />} />
+            <Route path="/host" element={<Host />} />
+            <Route path="/chain" element={<Chain />} />
+            <Route path="/block" element={<Block />} />
+            <Route path="/simulation-settings" element={<SimulationSettings />} />
+            <Route path="/metrics" element={<Metrics />} />
+          </Routes>
+        </div>
+      </SideBar>
     </ChakraProvider>
   );
 }
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,75 @@
+import React, { ReactNode, ReactText } from 'react';
+import {
+  IconButton,
+  Box,
+  CloseButton,
+  Flex,
+  Icon,
+  useColorModeValue,
+  Link,
+  Drawer,
+  DrawerContent,
+  Text,
+  useDisclosure,
+  BoxProps,
+  FlexProps,
+} from '@chakra-ui/react';
+import {
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+  FiMenu,
+} from 'react-icons/fi';
+import { IconType } from 'react-icons';
+
+interface LinkItemProps {
+  name: string;
+  icon: IconType;
+}
+
+const LinkItems: Array<LinkItemProps> = [
+  { name: 'Home', icon: FiHome },
+  { name: 'Trending', icon: FiTrendingUp },
+  { name: 'Explore', icon: FiCompass },
+  { name: 'Favourites', icon: FiStar },
+  { name: 'Settings', icon: FiSettings },
+];
+
+interface SidebarProps extends BoxProps {
+  onClose: () => void;
+}
+
+function SidebarContent({ onClose, ...rest }: SidebarProps) {
+  return (
+    <Box
+      bg={useColorModeValue('white', 'gray.900')}
+      borderRight="1px"
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}
+    >
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Logo
+        </Text>
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+    </Box>
+  );
+}
+
+export default function SideBar({ children }: { children?: ReactNode }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {children}
+      </Box>
+    </Box>
+  );
+}
